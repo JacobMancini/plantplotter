2db04b957cc92565d902474939f16edf6e10742a
[Jacob Mancini] <[jmancini021@gmail.com]>
2023-06-05 15:07:18 +0930

    Added dragging but it's slow

--- src/components/PlantsPage.vue
+++ src/components/PlantsPage.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h1>Plant Plotter</h1>
         <table id="plantClick">
             <tr>
                 <td v-for="item in plants" :key="item.plant">
@@ -14,7 +15,7 @@
             </tr>
             <tr>
                 <td v-for="(value, key, index) in plants" :key="index">
-                    <input :id="key" type="text" v-model="plantCount" style="width: 40px">
+                    <input :id="key" type="text" v-model="plantCount" style="width: 40px" readonly>
                 </td>
             </tr>
         </table>
@@ -25,6 +26,9 @@
                 <td>Width: <input type ="text" name="plotWidth" v-model="plotWidth" style="width: 30px;" @input=changeSize></td>
                 <td>Height: <input type ="text" name="plotHeight" v-model="plotHeight" style="width: 30px;" @input=changeSize></td>
             </tr>
+        </table>
+        <br>
+        <table id="plotButtons">
             <tr>
                 <td>
                     <button v-on:click="clearPlot()">Clear Plot</button>
@@ -32,12 +36,12 @@
             </tr>
         </table>
         <br>
-        <div style="text-align: center; width: 100%">
+        <div style="text-align: center; width: 100%; height: 100%">
             <div style="display: inline-block;">
                 <v-stage :config = "configKonva" >
                     <v-layer>
                         <v-rect :config = "configPlot"></v-rect>
-                        <v-circle v-for="circle in plantCircles" :key="circle.id" :config = "circle"></v-circle>  
+                        <v-circle v-for="circle in plantCircles" :key="circle.id" :config = "circle" v-on:dragmove="dragging"></v-circle>  
                     </v-layer>
                 </v-stage>
             </div>
@@ -53,14 +57,13 @@ export default {
         return {
             plants: plantsInfo,
             
-            drag: true,
             plantCount: 0,
 
             // Default dimensions of garden
             plotWidth: 6,
             plotHeight: 4,
 
-            // Array of circles for plants    
+            // Array of circles for plants 
             plantCircles: [],
             plantOrder: [],
 
@@ -69,34 +72,19 @@ export default {
                 height: 400,
             },
 
-            // Default circle for plants. Will change dimensions of default circle to desired plant upon plant creation
-            configCircle: {
-                radius: 20,
-                x: 0,
-                y: 0,
-                fill: "red",
-                stroke: "black",
-                strokeWidth: 3,
-                draggable: true,
-                id: null,
-            },
-
             // Garden
             configPlot: {
                 x: 0,
                 y: 0,
                 width: 600,
                 height: 400,
-                fill: "brown",
+                fill: "rgb(175, 155, 125)",
                 stroke: "black",
                 strokeWidth: 3,
             },
         }
     },
-    mounted () {
-        
-    },
-    
+
     methods: {
         // +1 from plant count
         add (plantId) {
@@ -109,9 +97,11 @@ export default {
             if (document.getElementById(plantId).value > 0){
                 document.getElementById(plantId).value = Number(document.getElementById(plantId).value) - 1;
                 const indexOfPlant = this.plantOrder.lastIndexOf(plantId);
-                this.plantOrder.splice(indexOfPlant,1);
+                this.plantOrder.splice(indexOfPlant, 1);
                 this.plantCircles.splice(indexOfPlant, 1);
-                // Removing last instance of plant from array   
+                // Removing last instance of plant from array. plantOrder is an array that stores the plantId of each plant. 
+                // Upon removal, I am locating the most recently created plantId and am setting that to indexOfPlant, altering the key of plantCircles and plantOrder
+                // and then removing the latest instance of that and hence, plant, from plantOrder and plantCircles
             }
         },
 
@@ -123,6 +113,7 @@ export default {
 
         clearPlot () {
             this.plantCircles = []
+            this.plantOrder = []
             this.plantCount = -1
             if (this.plantCount == -1) {
                 this.plantCount = 0
@@ -135,50 +126,76 @@ export default {
 
         createCircle (plantId) {
             this.plantCircles.push({
-                    "radius": this.plants[plantId].radius,
-                    "x": Math.floor(Math.random() * (this.configPlot.width - 2 * (this.plants[plantId].radius)) + this.plants[plantId].radius),
-                    "y": Math.floor(Math.random() * (this.configPlot.height - 2 * (this.plants[plantId].radius)) + this.plants[plantId].radius),
-                    "fill": this.plants[plantId].colour,
-                    "stroke": "black",
-                    "strokeWidth": 2,
-                    "draggable": true,
-                    "id": String(this.plants[plantId].id), // Konva does not like id being a number
-                    // "id:" this.plants[plantId].id returns:
-                    //Konva warning: 9 is a not valid value for "id" attribute. The value should be a string.
-                }); 
-            },  
+                "x": Math.floor(Math.random() * (this.configPlot.width - 2 * this.plants[plantId].radius) + this.plants[plantId].radius),
+                "y": Math.floor(Math.random() * (this.configPlot.height - 2 * this.plants[plantId].radius) + this.plants[plantId].radius),
+                "radius": this.plants[plantId].radius,
+                "name": this.plants[plantId].plant,
+                "fill": this.plants[plantId].colour,
+                "stroke": "black",
+                "strokeWidth": 2,
+                "draggable": true,
+                "id": String(this.plants[plantId].id), 
+            }); 
+        },  
 
-        dragging () { // Using a while loop and drag bool freezes the application. Can I call dragging somewhere other than v-circle?
-            if (this.plantCircles.x < (this.configPlot.width - this.plantCircles.radius) && this.plantCircles.y < (this.configPlot.height - this.plantCircles.radius)) {
-            this.plantCircles.draggable = true
-            }
-            else {
-                this.plantCircles.draggable = false
-                this.plantCircles.x = this.plantCircles.x - this.plantCircles.radius
-                this.plantCircles.y = this.plantCircles.y - this.plantCircles.radius
-            }
+        dragging(drag) {
+            // Finding the id of the circle that is being dragged
+            const circle = this.plantCircles.find((circle) => circle.id === drag.target.id());
+
+            // Getting new x and y position of circle and ensuring the circle stays inside the garden
+            const newX = Math.min(Math.max(circle.radius, drag.target.x()), this.configPlot.width - circle.radius);
+            const newY = Math.min(Math.max(circle.radius, drag.target.y()), this.configPlot.height - circle.radius);
+
+            drag.target.setAttrs({
+                x: newX,
+                y: newY,
                 
-            }
-            
+            });      
 
-        }, 
+        },
+         
+        
+        }
     }
 </script>
 
 <style>
 
+html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    background-color: rgb(163, 209, 93);
+    
+}
+
+h1 {
+    margin-left: auto;
+    margin-right: auto;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 36px;
+    color: rgb(70, 100, 10);
+}
+
 
 table {
     margin-left: auto;
     margin-right: auto;
- 
     
 }
 
-#plotChange, td{
-    font-size: 16px;
+#plotChange {
+    font-size: 18px;
     font-family: Arial;
-    font-weight: 100;
+    font-weight: bold;
+    color: rgb(70, 100, 10);
+}
+
+#plantClick {
+    background-color: rgb(255, 255, 255, 0.2);
+    border-radius: 25px;
 }
 
 button {
